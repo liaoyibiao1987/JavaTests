03937964a7d495d3c771ed538c182dab6348ff3e
liaoyibiao1987 <zlyun616@gmail.com>
2016-02-22 18:24:55 +0800

    Signed-off-by: liaoyibiao1987 <zlyun616@gmail.com>

--- src/com/TestJava/TestJava.java
+++ src/com/TestJava/TestJava.java
@@ -1,10 +1,20 @@
 package com.TestJava;
 
+import java.net.URL;
+
 public class TestJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1111");
+		//TestJVM();
 	}
 
+	public static void TestJVM() {
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+		for (int i = 0; i < urls.length; i++) {
+			System.out.println(urls[i].toExternalForm());
+		}
+	}
 }
