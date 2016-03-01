de291496b00464845adf07f2429ab4a5060952a0
liaoyibiao1987 <zlyun616@gmail.com>
2016-03-01 23:15:57 +0800

    合并两个分支
    Signed-off-by: liaoyibiao1987 <zlyun616@gmail.com>

--- src/TestDefualtDelimiter.java
+++ src/TestDefualtDelimiter.java
@@ -3,6 +3,6 @@
 //的文件看作那个目录“默认包”的一部分，所以它们对于目录内的其他文件来说是“友好”的。
 public class TestDefualtDelimiter {
 	void TestDefualt() {
-		System.out.println("访问权限控制2");
+		System.out.println("访问权限控制2  测试到分支");
 	}
 }
