b395008249dc8bf0d241c3800804208c382bcf30
liaoyibiao1987 <zlyun616@gmail.com>
2016-03-09 23:22:20 +0800

    Signed-off-by: liaoyibiao1987 <zlyun616@gmail.com>

---
+++ src/com/keywords/Month2.java
@@ -0,0 +1,19 @@
+package com.keywords;
+//如确实不想放弃额外的类型安全性,又想实现接口的简便性
+public class Month2 {
+	private String name;
+
+	private Month2(String nm) {
+		name = nm;
+	}
+
+	public String toString() {
+		return name;
+	}
+
+	public final static Month2 JAN = new Month2("January"), FEB = new Month2("February"), MAR = new Month2("March"),
+			APR = new Month2("April"), MAY = new Month2("May"), JUN = new Month2("June"), JUL = new Month2("July"),
+			AUG = new Month2("August"), SEP = new Month2("September"), OCT = new Month2("October"),
+			NOV = new Month2("November"), DEC = new Month2("December");
+	public final static Month2[] month = { JAN, JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
+}
