1375cba55da684a7f70798868e9d904248b57599
liaoyibiao1987 <zlyun616@gmail.com>
2016-06-23 14:43:55 +0800

    Signed-off-by: liaoyibiao1987 <zlyun616@gmail.com>

--- src/com/keywords/UnnamedInnerClass.java
+++ src/com/keywords/UnnamedInnerClass.java
@@ -24,4 +24,84 @@ public class UnnamedInnerClass {
 		};
 		dog.beFriendly();
 	}
+
+	public Pet TestUnnamedInner2(final String dest, final float price) {
+		return new Pet() {
+
+			private String label = dest;
+
+			public String readLabel() {
+				return label;
+			}
+
+			private int cost;
+
+			// Instance initialization for each object:
+			// 实例化方法
+			{
+				// 四舍五入
+				cost = Math.round(price);
+				if (cost > 100)
+					System.out.println("Over budget!");
+			}
+
+			@Override
+			public void beFriendly() {
+				System.out.println("蹭蹭你^_^");
+			}
+
+			@Override
+			public void play() {
+				System.out.println("把飞盘叼给你，逼你把飞盘丢出去，然后它再捡回来让你继续扔，连续500次^_^");
+			}
+			// 錯誤的方法
+			/*
+			 * public String TestRead() { return desc; }
+			 */
+		};
+	}
+
+	public Pet TestUnnamedInner3() {
+
+		// 内部类想要访问外部类（方法）的变量，则变量必须为final
+		// String dest = "dest";
+		// float dest = 25;
+
+		final String dest = "dest";
+		final float price = 25f;
+
+		return new Pet() {
+
+			private String label = dest;
+
+			public String readLabel() {
+				return label;
+			}
+
+			private int cost;
+
+			// Instance initialization for each object:
+			// 实例化方法
+			{
+				// 四舍五入
+				cost = Math.round(price);
+				if (cost > 100)
+					System.out.println("Over budget!");
+			}
+
+			@Override
+			public void beFriendly() {
+				System.out.println("蹭蹭你^_^");
+			}
+
+			@Override
+			public void play() {
+				System.out.println("把飞盘叼给你，逼你把飞盘丢出去，然后它再捡回来让你继续扔，连续500次^_^");
+			}
+			// 錯誤的方法
+			/*
+			 * public String TestRead() { return desc; }
+			 */
+		};
+	}
 }
