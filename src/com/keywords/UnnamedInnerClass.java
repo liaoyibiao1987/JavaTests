2162c75e49829e5039c1bad75ea673f3fab04cf0
liaoyibiao1987 <zlyun616@gmail.com>
2016-06-23 14:35:53 +0800

    Signed-off-by: liaoyibiao1987 <zlyun616@gmail.com>

--- src/com/keywords/UnnamedInnerClass.java
+++ src/com/keywords/UnnamedInnerClass.java
@@ -1,5 +1,27 @@
 package com.keywords;
 
 public class UnnamedInnerClass {
+	public interface Pet {
 
+		public void beFriendly();
+
+		public void play();
+
+	}
+
+	public void TestUnnamedInner() {
+		Pet dog = new Pet() {
+
+			@Override
+			public void beFriendly() {
+				System.out.println("蹭蹭你^_^");
+			}
+
+			@Override
+			public void play() {
+				System.out.println("把飞盘叼给你，逼你把飞盘丢出去，然后它再捡回来让你继续扔，连续500次^_^");
+			}
+		};
+		dog.beFriendly();
+	}
 }
