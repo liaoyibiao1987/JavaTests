6fd88676db6ce3018afc32cb5e0d9aaac4e29c30
liaoyibiao1987 <zlyun616@gmail.com>
2016-08-26 09:32:34 +0800

    添加二号

---
+++ src/com/ppl/exceptionsTest/ThrowTwo.java
@@ -0,0 +1,22 @@
+package com.ppl.exceptionsTest;
+
+import com.sun.management.jmx.Trace;
+import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
+
+public class ThrowTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = null;
+		TestNull(x);
+	}
+
+	public static void TestNull(String x) {
+		try {
+			byte[] xy = x.getBytes();
+		} catch (Exception se) {
+			se.printStackTrace();
+			System.out.println(se + "ÀÏÂÞËµµÄ¡£");
+		}
+	}
+}
