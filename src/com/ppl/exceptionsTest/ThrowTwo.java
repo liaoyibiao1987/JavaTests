0efbf3b128b6d0ea0b95a23ab3e8ac71709ad4cf
liaoyibiao1987 <zlyun616@gmail.com>
2016-08-26 10:42:43 +0800

    在东耀到底

--- src/com/ppl/exceptionsTest/ThrowTwo.java
+++ src/com/ppl/exceptionsTest/ThrowTwo.java
@@ -3,11 +3,10 @@ package com.ppl.exceptionsTest;
 import com.sun.management.jmx.Trace;
 import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
 
-//好像家里的不是和这里的在一个频道上
 public class ThrowTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub   
 		String x = null;
 		TestNull(x);
 	}
