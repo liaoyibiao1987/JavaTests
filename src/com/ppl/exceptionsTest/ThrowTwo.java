c810499890d47a03584d7534ad51972751a3861f
liaoyibiao1987 <zlyun616@gmail.com>
2016-08-26 11:14:00 +0800

    是

--- src/com/ppl/exceptionsTest/ThrowTwo.java
+++ src/com/ppl/exceptionsTest/ThrowTwo.java
@@ -6,7 +6,7 @@ import com.sun.org.apache.xml.internal.resolver.helpers.Debug;
 public class ThrowTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub   
+		// TODO Auto-generated method stub
 		String x = null;
 		TestNull(x);
 	}
